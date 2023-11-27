c20a41d4b415563ca6f4fd6ee1587e7fc86c1e9f
Alexandre Junior <alexandrejunior408@gmail.com>
2023-11-27 12:38:06 -0300

    feat: creates first modules and controller to show how to use

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductModule } from './products/product.module';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductModule, UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/products/product.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+
+@Module({ imports: [], controllers: [ProductController], providers: [] })
+export class ProductModule {}
