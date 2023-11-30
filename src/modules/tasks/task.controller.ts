16e84cebb161caa640b5720846c5d40b97833fb7
Alexandre Junior <alexandrejunior408@gmail.com>
2023-11-30 14:04:51 -0300

    docs: create documentation

--- src/modules/tasks/task.controller.ts
+++ src/modules/tasks/task.controller.ts
@@ -2,14 +2,24 @@ import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 
 import { CreateTaskUserUseCase } from './use-cases/create-task-user.use-case';
 import { AuthGuard } from 'src/infrastructure/providers/auth-guard.provider';
-import { CreateTaskUserSchemaDTO } from './schemas/task-user.schema';
+import {
+  CreateTaskUserSchema,
+  CreateTaskUserSchemaDTO,
+} from './schemas/task-user.schema';
+import { zodToOpenAPI } from 'nestjs-zod';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
+
+const schemaTaskUserSwagger = zodToOpenAPI(CreateTaskUserSchema);
 
 @Controller('/tasks')
+@ApiTags('tasks')
 export class TaskUserController {
   constructor(private createTaskUserUseCase: CreateTaskUserUseCase) {}
 
   @Post()
+  @ApiBody({ description: 'Create Task User', schema: schemaTaskUserSwagger })
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
   async create(@Body() data: CreateTaskUserSchemaDTO, @Request() request) {
     return this.createTaskUserUseCase.execute({
       ...data,
--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -16,14 +16,26 @@ import { CreateUserValidationPipe } from './pipe/create-user.validation.pipe';
 import { ProfileUserUseCase } from './use-cases/profile-user.use-case';
 import {
   CreateUserResponseSchemaDTO,
+  CreateUserSchema,
   CreateUserSchemaDTO,
 } from './schemas/create-user.schema';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileDTO } from './dto/user.dto';
 import { UploadAvatarUserUseCase } from './use-cases/upload-avatar-user.use-case';
 import { AuthGuard } from '../../infrastructure/providers/auth-guard.provider';
+import { zodToOpenAPI } from 'nestjs-zod';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+
+const schemaUserSwagger = zodToOpenAPI(CreateUserSchema);
 
 @Controller('/users')
+@ApiTags('users')
 export class UserController {
   constructor(
     private readonly createUserUseCase: CreateUserUseCase,
@@ -32,6 +44,9 @@ export class UserController {
   ) {}
 
   @Post()
+  @ApiBody({ description: 'Create User', schema: schemaUserSwagger })
+  @ApiResponse({ status: 201, description: 'User created successfully' })
+  @ApiResponse({ status: 400, description: 'User already exists' })
   @UsePipes(new CreateUserValidationPipe())
   async create(@Body() data: CreateUserSchemaDTO) {
     const user = await this.createUserUseCase.execute(data);
@@ -40,6 +55,7 @@ export class UserController {
 
   @Get('/profile')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
   async profile(@Request() request) {
     return this.profileUserUseCase.execute(request.user.sub);
   }
@@ -47,12 +63,23 @@ export class UserController {
   @Put('/avatar')
   @UseInterceptors(FileInterceptor('file'))
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
   async uploadAvatar(@Request() request, @UploadedFile() file: FileDTO) {
-    const result = await this.uploadAvatarUserUseCase.execute({
+    return await this.uploadAvatarUserUseCase.execute({
       file,
       userId: request.user.sub,
     });
-
-    return result;
   }
 }
